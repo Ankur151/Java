561862976a732f0bc3c9f5ed181c1e9f5881898f
Ankur Vashist <ankurvashist99@gmail.com>
2018-07-12 23:46:47 +0530

    updated sort

--- src/Sort.java
+++ src/Sort.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 
 public class Sort {
 public static void main(String args[])
 {
-	int i,j,temp=0;
-	int A[]={1,1,1,1,0,0,1,0};
-	System.out.println("Given array: 1 1 1 1 0 0 1 0 ");
+	
+	int i,j,l,temp=0;
+	int A[]= new int[5];
+	System.out.println("Enter the elements of array");
+	Scanner sn=new Scanner(System.in);
+	for(l=0;l<5;l++)
+	{
+		
+		A[l]= sn.nextInt();
+		
+	}
+	sn.close();
+	//System.out.println("Given array: 1 1 1 1 0 0 1 0 ");
 	for(i=0;i<A.length-2;i++)
 	{
 		for(j=i+1;j<A.length;j++)
