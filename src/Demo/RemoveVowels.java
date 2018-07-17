e8e63b3576c26be427a4625ef7cef2cde4c69fcb
Ankur Vashist <ankurvashist99@gmail.com>
2018-07-17 21:50:06 +0530

    8th Assignment

---
+++ src/Demo/RemoveVowels.java
@@ -0,0 +1,16 @@
+package Demo;
+import java.util.Scanner;
+public class RemoveVowels{
+ public static void main(String[] args) {
+ 
+	 System.out.println("Enter the String");
+		Scanner in=new Scanner(System.in);
+		String array=in.nextLine();
+		in.close();
+	 
+        array=array.replaceAll("[aeiouAEIOU]", "");
+        System.out.println(array);
+ 
+    }
+ 
+}
---
+++ src/Demo/abc.java
@@ -0,0 +1,22 @@
+package Demo;
+
+public class abc {
+	public static void main(String ag[]) {
+		int i,j;
+		
+		for(i=0;i<3;i++)
+		{
+			char c='a';
+			for(j=0;j<=i;j++)
+			{
+				System.out.print(c);
+				c++;
+				
+			}
+		System.out.println("\n");
+			
+		}
+	}
+}
+
+
